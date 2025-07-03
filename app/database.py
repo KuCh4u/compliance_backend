7d2011ddcb690fb4bfa527154de1ab5973382e64
nicolas Cartagena <nicolas.a.cartagena.m@gmail.com>
2025-07-02 23:58:07 -0400

    change config

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost/compliance")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@db/compliance")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
