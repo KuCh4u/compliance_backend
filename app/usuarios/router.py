937fe14b2279510d869e541a1519626e21b20bd7
nicolas Cartagena <nicolas.a.cartagena.m@gmail.com>
2025-07-04 23:27:31 -0400

    fix

--- app/usuarios/router.py
+++ app/usuarios/router.py
@@ -10,14 +10,14 @@ router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 @router.post("/login", response_model=TokenResponse)
 async def login(request: LoginRequest, db: AsyncSession = Depends(get_db)):
     try:
-        if not await verify_recaptcha(request.recaptcha_token) and False:
-            raise HTTPException(status_code=400, detail="reCAPTCHA inválido")
+        # if not await verify_recaptcha(request.recaptcha_token) and False:
+        #     raise HTTPException(status_code=400, detail="reCAPTCHA inválido")
         token = await login_user(db, request.email, request.password)
-        if not token or token == False:
-            raise HTTPException(status_code=401, detail="Correo o contraseña incorrectos")
+        # if not token or token == False:
+        #     raise HTTPException(status_code=401, detail="Correo o contraseña incorrectos")
         return token
     except Exception as e:
-        pass
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.post("/register", response_model=UserOut)
 async def register_usuario(usuario: CreateUser, db: AsyncSession = Depends(get_db)):
